8ba10b88dc04afe8048ddfca34144bb902846f1e
MikenugServices <mikenugfc2024@gmail.com>
2025-06-15 14:27:19 +0300

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -33,7 +33,7 @@ self.addEventListener("activate", async (e) => {
             const response = await saveSubscription(subscription);
             console.log(response)
             if(response === "Successfully saved") {
-                self.registration.showNotification("MIKENUG FC", {
+                self.registration.showNotification("Activation complete", {
                     body: "Notifications enabled!",
                     icon: "https://mikenug.halalas-services.xyz/imgs/mikenug.png"
                 })
